cfcefeb0a8cd1290db6bac746d5e1db96b83a6ec
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2022-11-10 18:09:16 +0900

    Create switch-case.c

---
+++ switch-case.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>  
+
+int main(void)
+{
+ int n=2;
+ switch(3)
+	{
+		case 0:
+			printf("?");
+			break;
+		case 1:
+			printf("??");
+			break;
+		case 2:
+			printf("???"); 
+			break;
+		case 3:
+			printf("????") ;
+			break;
+	}
+
+	return 0;
+}
