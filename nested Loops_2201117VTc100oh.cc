3c56af67587689c72bf820bf99c20d0a43e03c4e
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2022-11-17 18:43:53 +0900

    Update nested Loops_2201117VTc100oh.cc

--- nested Loops_2201117VTc100oh.cc
+++ nested Loops_2201117VTc100oh.cc
@@ -7,6 +7,14 @@ int main(void)
 	
 	scanf("%d",&a);
 	
+	/*
+	if a=5
+	1 2 3 4 5
+	1 2 3 4
+	1 2 3
+	1 2
+	1
+	*/
 	for(j=1;j<=a;j++)
 	{
 		for(i=1;i<=j;i++)
@@ -17,5 +25,25 @@ int main(void)
 		printf("\n");
 	}
 
+	
+	/*
+	5 4 3 2 1
+	4 3 2 1
+	3 2 1
+	2 1
+	1
+	*/
+	for(j=a;j>=1;j--)
+	{
+		for(i=j;i>=1;i--)
+		{
+			printf("%d ",i)	;
+		}
+		printf("\n");
+	}
 	return 0;
 }
+
+
+
+
