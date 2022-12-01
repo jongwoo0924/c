6470daf65d06d96c7ac688798d8dc1bebe90d24a
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2022-12-01 18:01:33 +0900

    Create C practice_1_20221201.c

---
+++ C practice_1_20221201.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ 
+int main(void) 
+{
+	int a = 12;
+	int b = 7;
+	float c = 58.3;
+	printf("Hello\n");
+	printf("\"World\"\n");
+	printf("(%d문제 중 ",a);
+	printf("%d 문제 정답:",b);
+	printf("%.1f%%)",c);
+	return 0;
+}
