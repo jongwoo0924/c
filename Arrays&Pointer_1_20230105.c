6a81add0361e8a253004af72215e7264cbc7a3ef
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2023-01-05 18:33:34 +0900

    Update Arrays&Pointer_1_20230105.c

--- Arrays&Pointer_1_20230105.c
+++ Arrays&Pointer_1_20230105.c
@@ -13,9 +13,9 @@
   printf("\n%u\n",&arr[0]);
   printf("\n%u\n",&arr[1]);
   printf("\n%u\n\n",&arr[2]);
-  printf("\n%d\n",arr);      //arr은 그 안의 값이 아닌 주소값을 의미
-  printf("\n%d\n",arr+1);    //arr+1 은 arr의 주소값 +4 즉 arr[2]를 뜻한다
-  printf("\n%d\n\n",arr+2);  //
+  printf("\n%u\n",arr);      //arr은 그 안의 값이 아닌 주소값을 의미
+  printf("\n%u\n",arr+1);    //arr+1 은 arr의 주소값 +4 즉 arr[2]를 뜻한다
+  printf("\n%u\n\n",arr+2);  //
   printf("\n%d %d\n", arr[0], *(arr)+0);      //arr[0]은 arr의 첫번째 값을 직접적으로 의미함
   printf("\n%d %d\n", arr[1], *(arr)+1);      //*(arr)은 arr의 첫번째 값을 주소값으로 간접적으로 나타냄
   printf("\n%d %d\n\n", arr[2], *(arr)+2);    //*(arr)+1은 arr의 첫째 값에 1을 더함
