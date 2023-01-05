85c4b9f3874bdfff9750aa98f40f1daa680123ef
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2023-01-05 18:23:14 +0900

    Update and rename Arrays&Pointer_1_20230105 to Arrays&Pointer_1_20230105.c

---
+++ Arrays&Pointer_1_20230105.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main (void)
+ {
+    /*  int a = 10;
+  int *p = &a;
+  printf(" a = %d\n",a);
+  printf(" p = %d\n",*p);
+  printf("&a = %u\n",&a);*/   //%u = unsigned 부호를 없앤다 ex) printf("%u",-1) => 1
+
+  int arr[3] = {10,20,30};
+  printf("\n%u\n",&arr[0]);
+  printf("\n%u\n",&arr[1]);
+  printf("\n%u\n\n",&arr[2]);
+  printf("\n%d\n",arr);      //arr은 그 안의 값이 아닌 주소값을 의미
+  printf("\n%d\n",arr+1);    //arr+1 은 arr의 주소값 +4 즉 arr[2]를 뜻한다
+  printf("\n%d\n\n",arr+2);  //
+  printf("\n%d %d\n", arr[0], *(arr)+0);      //arr[0]은 arr의 첫번째 값을 직접적으로 의미함
+  printf("\n%d %d\n", arr[1], *(arr)+1);      //*(arr)은 arr의 첫번째 값을 주소값으로 간접적으로 나타냄
+  printf("\n%d %d\n\n", arr[2], *(arr)+2);    //*(arr)+1은 arr의 첫째 값에 1을 더함
+  
+
+  return 0;
+ }
